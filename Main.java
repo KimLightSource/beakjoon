926e7b9a08241ae059833dbf8a682750efd3c483
gwkim <gwkim@softnet.co.kr>
2023-03-20 17:36:05 +0900

    25206번 - 너의 평점은

--- Main.java
+++ Main.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String word = bufferedReader.readLine();
-        word = word.toUpperCase();
-        int[] ints = new int[26];
-        for (char c : word.toCharArray()) {
-            ints[c - 65]++;
-        }
-        int count = 0;
-        int answer = 0;
-        for (int i = 0; i < 26; i++) {
-            if (ints[i] > count) {
-                count = ints[i];
-                answer = i+65;
-            } else if (ints[i] == count) {
-                answer = 63;
+        Scanner sc = new Scanner(System.in);
+        double totalScore = 0;
+        double totalCredit = 0;
+        for (int i = 0; i < 20; i++) {
+            String subject = sc.next();
+            double credit = sc.nextDouble();
+            String score = sc.next();
+            if (score.equals("A+")) {
+                totalScore = totalScore + (credit * 4.5);
+                totalCredit = totalCredit+credit;
             }
+            else if (score.equals("A0")) {totalScore = totalScore + (credit * 4.0); totalCredit = totalCredit+credit;}
+            else if (score.equals("B+")) {totalScore = totalScore + (credit * 3.5); totalCredit = totalCredit+credit;}
+            else if (score.equals("B0")) {totalScore = totalScore + (credit * 3.0); totalCredit = totalCredit+credit;}
+            else if (score.equals("C+")) {totalScore = totalScore + (credit * 2.5); totalCredit = totalCredit+credit;}
+            else if (score.equals("C0")) {totalScore = totalScore + (credit * 2.0); totalCredit = totalCredit+credit;}
+            else if (score.equals("D+")) {totalScore = totalScore + (credit * 1.5); totalCredit = totalCredit+credit;}
+            else if (score.equals("D0")) {totalScore = totalScore + (credit * 1.0); totalCredit = totalCredit+credit;}
+            else if (score.equals("F")) {totalScore = totalScore + (credit * 0); totalCredit = totalCredit+credit;}
+            else if (score.equals("P") ) totalScore = totalScore;
         }
-        System.out.println((char) answer);
+        System.out.println(totalScore /totalCredit);
+
+
     }
 }
