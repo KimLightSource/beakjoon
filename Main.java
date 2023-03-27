bf564d50edf92e12545eba5dea3337f71b69be3e
gwkim <gwkim@softnet.co.kr>
2023-03-27 17:18:55 +0900

    백준 9606번 - 약수들의 합

--- Main.java
+++ Main.java
@@ -1,24 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int i = Integer.parseInt(st.nextToken());
-        int j = Integer.parseInt(st.nextToken());
-        int count = 0;
-        for (int a = 1; a <= i; a++) {
-            if (i % a == 0) count++;
-            if (count == j) {
-                System.out.println(a);
-                break;
+        int i = 0;
+        while (true) {
+            String answer = "";
+            int b = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            i = Integer.parseInt(st.nextToken());
+            if (i == -1) break;
+            answer = i+" = 1";
+            for (int a = 2; a < i; a++) {
+                if (i % a == 0) b = a + b;
             }
+            if (i == b+1) {
+                for (int a = 2; a < i; a++) {
+                    if (i % a == 0) {answer = answer + " + " +a ; }
+            }} else if (i != b+1) {
+                answer = i + " is NOT perfect.";
+            }
+            System.out.println(answer);
         }
-        if (count < j) System.out.println(0);
+
 
     }
 
