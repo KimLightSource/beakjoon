ace82cab694ccccb712e6254e16cb50d1de38047
gwkim <gwkim@softnet.co.kr>
2023-03-31 13:31:06 +0900

    백준 1269번 - 대칭 차집합

--- Main.java
+++ Main.java
@@ -1,28 +1,32 @@
 import java.io.*;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        HashMap<String, Integer> map = new HashMap<>();
-        ArrayList<Object> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
+        HashMap<String, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int i = Integer.parseInt(st.nextToken());
         int j = Integer.parseInt(st.nextToken());
-        for (int a = 1; a <= i; a++) {
-            String s = br.readLine();
-            map.put(s, a);
-            list.add(s);
+        String[] s1 = br.readLine().split(" ");
+        String[] s2 = br.readLine().split(" ");
+        for (int a = 0; a < i; a++) {
+            map1.put(s1[a], 0);
         }
-        for (int a = 1; a <= j; a++) {
-            String s = br.readLine();
-            if (map.containsKey(s)) sb.append(map.get(s)).append("\n");
-            else {
-                sb.append(list.get(Integer.parseInt(s)-1)).append("\n");}
+        for (int a = 0; a < j; a++) {
+            map2.put(s2[a], 0);
         }
-        bw.write(sb.toString());
+        int answer = 0;
+        for (int a = 0; a < i; a++) {
+            if (!map2.containsKey(s1[a])) answer++;
+        }
+        for (int a = 0; a < j; a++) {
+            if (!map1.containsKey(s2[a])) answer++;
+        }
+        bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
         br.close();
