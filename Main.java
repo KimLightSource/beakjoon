130c51556a2129120d7c918a904132f814b34e3a
gwkim <gwkim@softnet.co.kr>
2023-03-31 14:04:01 +0900

    백준 11478번 - 서로 다른 부분 문자열의 개수

--- Main.java
+++ Main.java
@@ -6,27 +6,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<String, Integer> map2 = new HashMap<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int i = Integer.parseInt(st.nextToken());
-        int j = Integer.parseInt(st.nextToken());
-        String[] s1 = br.readLine().split(" ");
-        String[] s2 = br.readLine().split(" ");
-        for (int a = 0; a < i; a++) {
-            map1.put(s1[a], 0);
+        String s = br.readLine();
+        HashSet<Object> set = new HashSet<>();
+        for (int a = 0; a < s.length(); a++) {
+            for (int b = a + 1; b <= s.length(); b++) {
+                set.add(s.substring(a, b));
+            }
         }
-        for (int a = 0; a < j; a++) {
-            map2.put(s2[a], 0);
-        }
-        int answer = 0;
-        for (int a = 0; a < i; a++) {
-            if (!map2.containsKey(s1[a])) answer++;
-        }
-        for (int a = 0; a < j; a++) {
-            if (!map1.containsKey(s2[a])) answer++;
-        }
-        bw.write(String.valueOf(answer));
+        bw.write(String.valueOf(set.size()));
         bw.flush();
         bw.close();
         br.close();
