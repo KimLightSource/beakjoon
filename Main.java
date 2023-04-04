50056ce82f418a53d68b83252ea88dd06c5464bc
gwkim <gwkim@softnet.co.kr>
2023-04-04 16:49:05 +0900

    백준 2485번 - 가로수

--- Main.java
+++ Main.java
@@ -6,14 +6,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String s = br.readLine();
-        HashSet<Object> set = new HashSet<>();
-        for (int a = 0; a < s.length(); a++) {
-            for (int b = a + 1; b <= s.length(); b++) {
-                set.add(s.substring(a, b));
-            }
+        int i = Integer.parseInt(br.readLine());
+        int[] j = new int[i];
+        for (int a = 0; a < i; a++) {
+            j[a] = Integer.parseInt(br.readLine());
         }
-        bw.write(String.valueOf(set.size()));
+        ArrayList<Integer> agbs = new ArrayList<>();
+        for (int a = 0; a < j.length - 2; a++) {
+            int b = j[a + 1] - j[a];
+            int c = j[a + 2] - j[a + 1];
+            while (c > 0) {
+                int temp = b;
+                b = c;
+                c = temp%c;
+            }
+            agbs.add(b);
+        }
+        Integer agb = agbs.stream().min(Comparator.comparing(x -> x)).get();
+        int answer = 0;
+        int count = j[0];
+        for (int a = 1; a < j.length; a++) {
+            count = count + agb;
+            if (count != j[a]) {
+                answer++;
+                a--;
+            } else continue;
+
+        }
+
+        bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
         br.close();
