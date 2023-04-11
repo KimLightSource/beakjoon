05f420bfedfada7e2db044cf807c0f35f07d3d67
gwkim <gwkim@softnet.co.kr>
2023-04-11 15:10:49 +0900

    백준 2164번 - 카드

--- Main.java
+++ Main.java
@@ -1,44 +1,31 @@
 import java.io.*;
-import java.sql.Array;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        while (!s.equals(".")) {
-            Stack<Character> stack = new Stack<>();
-            String answer = "yes";
-            for (char c : s.toCharArray()) {
-                if (c == '(') {
-                    stack.push('(');
-                } else if (c == '[') {
-                    stack.push('[');
-                } else if (c == ')') {
-                    if (stack.isEmpty()) {
-                        answer = "no";
-                        break;
-                    }
-                    if (stack.pop() != '(') {answer = "no"; break;}
-                }else if (c == ']') {
-                    if (stack.isEmpty()) {
-                        answer = "no";
-                        break;
-                    }
-                    if (stack.pop() != '[') {answer = "no"; break;}
-                }
-            }
-            if (!stack.isEmpty()) answer = "no";
-            sb.append(answer).append("\n");
-            s= br.readLine();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int a = 1; a <= i; a++) {
+            queue.offer(a);
         }
+        while (queue.size() > 1) {
+            queue.remove();
+            Integer remove = queue.remove();
+            queue.offer(remove);
+        }
+        sb.append(queue.remove());
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
+
+
 
 }
 
