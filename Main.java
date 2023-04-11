821b58f70381eab0bf4f9621fbaa51acb5d875ef
gwkim <gwkim@softnet.co.kr>
2023-04-11 13:14:10 +0900

    백준 9012번 - 괄호

--- Main.java
+++ Main.java
@@ -3,77 +3,38 @@ import java.sql.Array;
 import java.util.*;
 
 public class Main {
-
-    public static int[] stack;
-    public static int size = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int i = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        stack = new int[i];
-        for (int a = 0; a < i; a++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            String s = st.nextToken();
-            switch (s) {
-                case "push":
-                    push(Integer.parseInt(st.nextToken()));
-                    break;
-                case "top":
-                    sb.append(top()).append("\n");
-                    break;
-                case "size":
-                    sb.append(size()).append("\n");
-                    break;
-                case "empty":
-                    sb.append(empty()).append("\n");
-                    break;
-                case "pop":
-                    sb.append(pop()).append("\n");
-                    break;
-            }
-        }
 
+        for (int a = 0; a < i; a++) {
+            Stack<Character> stack = new Stack<>();
+            char[] chars = br.readLine().toCharArray();
+            String s = "YES";
+            for (char c : chars) {
+                if (c == '(') {
+                    stack.push(c);
+                }
+                else if (c == ')' && stack.isEmpty()) {
+                    s = "NO";
+                    break;
+                }
+                else {
+                    stack.pop();
+                }
+            }
+            if (!stack.isEmpty()) {
+                s = "NO";
+            }
+            sb.append(s).append("\n");
+        }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static void push(int item) {
-        stack[size] = item;
-        size++;
-    }
-
-    public static int pop() {
-        if (size == 0) {
-            return -1;
-        } else {
-            int num = stack[size - 1];
-            size--;
-            return num;
-        }
-    }
-
-    public static int size() {
-        return size;
-    }
-
-    public static int empty() {
-        if (size == 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    public static int top() {
-        if (size == 0) {
-            return -1;
-        } else {
-            return stack[size-1];
-        }
-    }
 }
 
